7812e1e57d3dbd12024624efa1ee748c7bb0e01a
rbpatel1 <63330284+rbpatel1@users.noreply.github.com>
2020-05-24 17:28:57 +0800

    Add files via upload

---
+++ CSP2101_rpatel1/portfolio 2/rectangle.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+echo "Dimensions of rectangle"
+#open and displaying the content
+cat rectangle.txt
+echo " "
+echo "____________________________________________________"
+echo " "
+#Arranging <Name>,< Height>, <Width>, <Area> , <Colour> to Name: <Name>: Height: <height>: Width <Width>: Colour: <Colour> using "sed"
+sed -e 's/ /,/g' rectangle.txt | 
+sed -e 's/Rec/Name: Rec/g' | 
+sed -e 's/,/ height : /' | 
+sed -e 's/,/ width : /' | 
+sed -e 's/,[^,]*/ /' | 
+sed -e 's/,/Color : /' >> rectangle_f.txt
